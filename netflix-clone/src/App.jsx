cd044ae22000454bffdbf824d25627c2adaccb99
tifetu <tifetuyoni@gmail.com>
2025-05-07 18:04:13 +0300

    first commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className='app'>
+      <h1>Hello world</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
