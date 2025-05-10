0ad70bf6d2cdb5834e2edf573aeffc61e3e10c87
tifetu <tifetuyoni@gmail.com>
2025-05-10 12:20:21 +0300

    add banner

--- netflix-clone/src/commonService/requests.jsx
+++ netflix-clone/src/commonService/requests.jsx
@@ -1,9 +1,11 @@
-API_KEY = process.env.REACT_APP_API_KEY;
+
+const API_KEY = "8cc79c08f548f0b3584261bd06aee321";
+
 // Api endpoints
 const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
---
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./banner.css";
+import axios from "../../commonService/axios";
+import requests from "../../commonService/requests";
+function Banner() {
+  const [banner, setBanner] = useState([]);
+  useEffect(() => {
+    async function BannerData(params) {
+      try {
+        const response = await axios.get(requests.fetchNetflixOriginals);
+        console.log(response);
+        setBanner(
+          response.data.results[
+            Math.floor(Math.random() * response.data.results.length)
+          ]
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    BannerData();
+  }, []);
+  console.log(banner);
+  function truncate(str, n) {
+    return str?.length > n ? str.slice(0, n - 1) + "...." : str;
+  }
+  return (
+    <div className="banner_conttainer">
+      <div
+        className="banner_img"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: `url("https://image.tmdb.org/t/p/original/${
+            banner?.backdrop_path || banner?.poster_path
+          }")`,
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="banner_contents">
+          <h1>{banner?.title || banner?.name || banner?.original_name}</h1>
+          <div className="banner_buttons">
+            <button className="banner_button">Play</button>
+            <button className="banner_button">My List</button>
+          </div>
+
+          <p className="banner_description">
+            {truncate(banner?.overview, 150)}
+          </p>
+        </div>
+        <div className="banner_fadebotom"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
